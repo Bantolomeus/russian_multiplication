13009ebb700f09852d93246504078efafc19e6c7
Christian Bantel <christian.bantel@dbfuhrpark.com>
2018-06-07 07:24:36 +0200

    Initial commit

---
+++ src/main/kotlin/com/bantolomeus/russian_multiplication/RussianMultiplicationApplication.kt
@@ -0,0 +1,11 @@
+package com.bantolomeus.russian_multiplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RussianMultiplicationApplication
+
+fun main(args: Array<String>) {
+    runApplication<RussianMultiplicationApplication>(*args)
+}
